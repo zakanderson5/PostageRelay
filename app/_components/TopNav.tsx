cbca0875a1fdb4edc916bc2e70b33a8050632365
Zak Anderson <zakanderson5@gmail.com>
2026-02-17 16:35:31 -0900

    Add TopNav component

---
+++ app/_components/TopNav.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function TopNav() {
+  return (
+    <header className="nav">
+      <div className="container navInner">
+        <Link href="/" className="brand">
+          <span className="brandMark">PR</span>
+          <span>PostageRelay</span>
+          <span className="badge">BETA</span>
+        </Link>
+
+        <nav className="navLinks">
+          <Link href="/how-it-works">How it works</Link>
+          <Link href="/pricing">Pricing</Link>
+          <Link href="/faq">FAQ</Link>
+        </nav>
+
+        <div className="navCtas">
+          <Link href="/login" className="btn">Log in</Link>
+          <Link href="/start" className="btn btnPrimary">Sign up</Link>
+        </div>
+      </div>
+    </header>
+  );
+}
